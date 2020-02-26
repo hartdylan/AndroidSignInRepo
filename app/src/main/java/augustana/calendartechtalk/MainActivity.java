185396e128361d87c3df06da14a4914db8eca98b
Dylan Hart <dylanhart16@augustana.edu>
2020-02-26 11:50:05 -0600

    Adding functionality of adding an event to the calendar.

--- app/src/main/java/augustana/calendartechtalk/MainActivity.java
+++ app/src/main/java/augustana/calendartechtalk/MainActivity.java
@@ -2,13 +2,48 @@ package augustana.calendartechtalk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b = findViewById(R.id.button);
+        b.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        setEvent();
+    }
+
+
+    public void setEvent() {
+        Calendar c = Calendar.getInstance();
+        c.set(2020, 6, 20, 7, 30);
+        Calendar endTime = Calendar.getInstance();
+        endTime.set(2020, 6, 20, 8, 30);
+        Intent intent = new Intent(Intent.ACTION_INSERT)
+                .setData(CalendarContract.Events.CONTENT_URI)
+                .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, c.getTimeInMillis())
+                .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endTime.getTimeInMillis())
+                .putExtra(CalendarContract.Events.TITLE, "Dylan's birthday.")
+                .putExtra(CalendarContract.Events.DESCRIPTION, "Celebration of Dylan's 22nd birthday!")
+                .putExtra(CalendarContract.Events.EVENT_LOCATION, "TBD")
+                .putExtra(CalendarContract.Events.AVAILABILITY, CalendarContract.Events.AVAILABILITY_BUSY)
+                .putExtra(Intent.EXTRA_EMAIL, "dylanhart16@augustana.edu");
+        startActivity(intent);
     }
 }
