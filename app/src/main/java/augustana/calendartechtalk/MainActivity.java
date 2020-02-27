7bcfa89a082bbadd2b876aa080e17254b67898f9
Dylan Hart <dylanhart16@augustana.edu>
2020-02-27 13:29:29 -0600

    Made the functionality of google API sign-on work.

--- app/src/main/java/augustana/calendartechtalk/MainActivity.java
+++ app/src/main/java/augustana/calendartechtalk/MainActivity.java
@@ -1,49 +1,81 @@
 package augustana.calendartechtalk;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.CalendarContract;
 import android.view.View;
-import android.widget.Button;
+import android.widget.Toast;
 
-import java.util.Calendar;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
-    Button b;
+
+public class MainActivity extends AppCompatActivity {
+
+    SignInButton b;
+    GoogleSignInClient c;
+    int RC_SIGN_IN = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        b = findViewById(R.id.button);
-        b.setOnClickListener(this);
+        b = findViewById(R.id.sign_in_button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.sign_in_button:
+                        signIn();
+                        break;
+                }
+            }
+        });
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        c = GoogleSignIn.getClient(this, gso);
 
     }
 
+    private void signIn() {
+        Intent signInIntent = c.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
 
     @Override
-    public void onClick(View v) {
-        setEvent();
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
     }
 
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            // Signed in successfully, show authenticated UI.
+            Toast.makeText(this, "Signed in successfully!", Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(MainActivity.this, Main2Activity.class);
+            startActivity(i);
 
-    public void setEvent() {
-        Calendar c = Calendar.getInstance();
-        c.set(2020, 6, 20, 7, 30);
-        Calendar endTime = Calendar.getInstance();
-        endTime.set(2020, 6, 20, 8, 30);
-        Intent intent = new Intent(Intent.ACTION_INSERT)
-                .setData(CalendarContract.Events.CONTENT_URI)
-                .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, c.getTimeInMillis())
-                .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endTime.getTimeInMillis())
-                .putExtra(CalendarContract.Events.TITLE, "Dylan's birthday.")
-                .putExtra(CalendarContract.Events.DESCRIPTION, "Celebration of Dylan's 22nd birthday!")
-                .putExtra(CalendarContract.Events.EVENT_LOCATION, "TBD")
-                .putExtra(CalendarContract.Events.AVAILABILITY, CalendarContract.Events.AVAILABILITY_BUSY)
-                .putExtra(Intent.EXTRA_EMAIL, "dylanhart16@augustana.edu");
-        startActivity(intent);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Toast.makeText(this, "Sign in failed!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
